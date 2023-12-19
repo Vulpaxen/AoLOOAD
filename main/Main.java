108312214bf25cf6eab7bb192ed4b701ae8b450b
Ronariv <96355410+Ronariv@users.noreply.github.com>
2023-12-19 15:07:05 +0700

    origin

--- main/Main.java
+++ main/Main.java
@@ -3,6 +3,8 @@ package main;
 import javafx.application.Application;
 import javafx.stage.Stage;
 import view.Authentication;
+import view.CashierPanel;
+import view.ChefPanel;
 import view.CustomerPanel;
 
 public class Main extends Application {
@@ -16,7 +18,11 @@ public class Main extends Application {
 //        Authentication authentication = new Authentication();
 //        authentication.show();
         
-        CustomerPanel cust = new CustomerPanel();
-        cust.show();
+//        CustomerPanel cust = new CustomerPanel();
+//        cust.show();
+    	CashierPanel cashier = new CashierPanel();
+    	cashier.show();
+//    	ChefPanel chef = new ChefPanel();
+//    	chef.show();
     }
 }
